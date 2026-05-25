2f654b04f95db6d578acf9ba1ab077c532404917
prince kumar <thunderrock497@gmail.com>
2026-05-25 17:04:20 +0530

    user model fix

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -42,7 +42,6 @@ const userSchema = new mongoose.Schema(
       type: String,
     },
 
-    // ── Patient specific fields ──
     age: {
       type: Number,
     },
@@ -64,7 +63,6 @@ const userSchema = new mongoose.Schema(
       type: Number,
     },
 
-    // ── Doctor specific fields ──
     specialisation: {
       type: String,
     },
